c8769125d4a33467d090d8db7e604ce17032ad42
elbitpmoC <bazellMP@gmail.com>
2021-03-30 22:55:22 -0400

    Setup.

---
+++ src/data/nodeTreeData.js
@@ -0,0 +1,37 @@
+let tree = {
+    "10": {
+        value: "10",
+        left: "4",
+        right: "17",
+    },
+    "4": {
+        value: "4",
+        left: "1",
+        right: "9",
+    },
+    "17": {
+        value: "17",
+        left: "12",
+        right: "18",
+    },
+    "1": {
+        value: "1",
+        left: null,
+        right: null,
+    },
+    "9": {
+        value: "9",
+        left: null,
+        right: null,
+    },
+    "12": {
+        value: "12",
+        left: null,
+        right: null,
+    },
+    "18": {
+        value: "18",
+        left: null,
+        right: null,
+    },
+};
