fb9bc3409a6c5493eb07fbe44848f17d4ad4669a
elbitpmoC <bazellMP@gmail.com>
2021-04-04 19:31:21 -0400

    formatting

--- src/code/chapter/04_data.js
+++ src/code/chapter/04_data.js
@@ -1,19 +1,20 @@
 var journal = [];
 
 function addEntry(events, squirrel) {
-  journal.push({events, squirrel});
+  journal.push({ events, squirrel });
 }
 
 function phi(table) {
   return (table[3] * table[0] - table[2] * table[1]) /
     Math.sqrt((table[2] + table[3]) *
-              (table[0] + table[1]) *
-              (table[1] + table[3]) *
-              (table[0] + table[2]));
+      (table[0] + table[1]) *
+      (table[1] + table[3]) *
+      (table[0] + table[2]));
 }
 
 function tableFor(event, journal) {
   let table = [0, 0, 0, 0];
+
   for (let i = 0; i < journal.length; i++) {
     let entry = journal[i], index = 0;
     if (entry.events.includes(event)) index += 1;
