e1e3eccc3ffe30588ee46c085e49d95c26c298c8
elbitpmoC <bazellMP@gmail.com>
2021-04-04 13:25:17 -0400

    fundamentals ✅ definitions, understanding, recap, etc.

---
+++ src/data/fundamentals.js
@@ -0,0 +1,7 @@
+// Data Structures(DS)
+// A way of efiiciently organizing data to be used effectively.
+// Key for creating fast & powerful algorithms.
+// Clean and easily understood code.
+
+// Abstract Data Type (ADT)
+//git 
