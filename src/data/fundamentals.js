bd5749fd2128eb670cb04f59a93d2af8732a2cec
elbitpmoC <bazellMP@gmail.com>
2021-04-04 13:23:11 -0400

    Stacks practice

--- src/data/fundamentals.js
+++
@@ -1,7 +0,0 @@
-// Data Structures(DS)
-// A way of efiiciently organizing data to be used effectively.
-// Key for creating fast & powerful algorithms.
-// Clean and easily understood code.
-
-// Abstract Data Type (ADT)
-// 
